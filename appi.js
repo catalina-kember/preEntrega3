012f65acc7c55fe1a08c222bb6cc8ba407bf52fc
Catalina Kember <catykember@gmail.com>
2023-07-26 10:20:30 -0300

    cambios 26-7

--- appi.js
+++ appi.js
@@ -1,6 +1,6 @@
 
 const button = document.querySelector("#btninfo");
-const apykey = a56d92332634edea0b49f338f976e4c5
+const apykey = "a56d92332634edea0b49f338f976e4c5"
 const url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + apykey;
 button.addEventListener("click", () => {
     fetch('https://jsonplaceholder.typicode.com/posts/1')
@@ -15,10 +15,14 @@ button.addEventListener("click", () => {
                     articles = data.articles;
 
                     for (i = 0; i < articles.length; i++) {
-                        console.log("Title: " + articles[i]['title']);
-                        console.log("Description: " + articles[i]['description']);
-            
-                        break;
+                        tarjeta.classList.add("tarjeta");
+                        tarjeta.innerHTML = ` 
+                                            <div class="tarjeta_informacion">
+                                                <span class="tarjeta_titulo">Titulo: ${articles[i]['title']}</span>
+                                                <span class="tarjeta_descripcion">Descripcion: $${articles[i]['description']}</span>
+                                            </div>
+                        `   
+                    break;
                     }
                 });
         })
